3547658378369ed75b7a5da18bdd071bf954c232
Abhishek <shashankmishrahjp@gmail.com>
2025-11-30 14:36:27 +0530

    feat: Final update of footer attribution to Abhishek Kumar.

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -3,10 +3,10 @@ const Footer = () => {
     <div className="text-center py-6 font-medium px-6 bg-neutral-900 text-neutral-200 text-sm sm:text-base">
       Made with ❤️ by
       <a
-        href="https://tanishka-deep.vercel.app/"
+        href="https://abhishekme.vercel.app/"
         className="ml-1 font-semibold underline"
       >
-        tanishkadeep
+        Abhishek Kumar
       </a>
     </div>
   );
